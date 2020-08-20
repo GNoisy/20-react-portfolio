d0b4b29a04235fb80d72302ea45d5f5deb1eb0c8
Giovanni Noisy <gnoisy77@gmail.com>
2020-08-19 19:08:36 -0700

    Updates

---
+++ precache-manifest.5bcb6c4bf5b121d9b58f6452f8a3a31e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9db1f40435abdd35c4376611fc8a5f33",
+    "url": "/20-react-portfolio/index.html"
+  },
+  {
+    "revision": "6dc43cc4933e07c439a2",
+    "url": "/20-react-portfolio/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "1c365c208adf2acaa09e",
+    "url": "/20-react-portfolio/static/css/main.eb8ae6a7.chunk.css"
+  },
+  {
+    "revision": "6dc43cc4933e07c439a2",
+    "url": "/20-react-portfolio/static/js/2.9b807b7b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/20-react-portfolio/static/js/2.9b807b7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c365c208adf2acaa09e",
+    "url": "/20-react-portfolio/static/js/main.8bbc1cce.chunk.js"
+  },
+  {
+    "revision": "3b0b0bc1435374e96fa3",
+    "url": "/20-react-portfolio/static/js/runtime-main.f1ddf74c.js"
+  }
+]);
